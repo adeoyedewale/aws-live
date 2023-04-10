83a8affcaaa8b1a64e9879e6006f349d0cc44b54
Adewale Oyedele <88012353+adeoyedewale@users.noreply.github.com>
2023-04-10 18:26:14 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employeedb.c5kx03zfr7wn.us-east-1.rds.amazonaws.com" 
 customuser = "eruobodo"
-custompass = "password123"
-customdb = "employeedb"
+custompass = "password123" #represents rds passwd
+customdb = "employee" #represents name of database created
 custombucket = "prod-employeedb"
 customregion = "us-east-1"
